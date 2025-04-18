ec7be16d1465433872f9907d7045aeed20f3bdcd
rayangdn <rayan.gauderon@epfl.ch>
2025-04-18 10:02:33 +0200

    init commit

---
+++ Assembly_students/example.py
@@ -0,0 +1,28 @@
+import matplotlib.pyplot as plt
+from rendering import plot_assembly_env, plot_task
+#from tree import  ExtendedTree, Action
+from tasks import Bridge
+from assembly_env import AssemblyEnv
+from blocks import Floor
+
+
+task = Bridge(num_stories=2)
+
+env = AssemblyEnv(task)
+done = False
+rewards = 0
+
+while not done:
+    action = env.random_action(non_colliding=True, stable=False)
+    if action is None:
+        break
+    print(action)
+    obs, r, done = env.step(action)
+    rewards += r
+    print(env.is_stable())
+    
+print(rewards)
+
+plot_assembly_env(env, task=task)
+plt.axis('equal')
+plt.show()
---
+++ Assembly_students/example_2.py
@@ -0,0 +1,61 @@
+import matplotlib.pyplot as plt
+from rendering import plot_assembly_env, plot_task
+from tasks import Bridge
+from assembly_env import AssemblyEnv, Action
+from blocks import Floor
+
+
+
+task = Bridge(num_stories=2)
+
+env = AssemblyEnv(task)
+
+action = Action(target_block=0, target_face=0, shape=1, face = 0, offset_x = -1)
+obs, r, done = env.step(action)
+
+for a in env.available_actions(num_block_offsets=5):
+    new_block = env.create_block(a)
+    if env.collision(new_block):
+        continue
+    env.add_block(new_block)  
+    if env.is_stable():
+        print(a)
+    env.delete_block(list(env.nodes())[-1])
+
+if 0:
+    action = Action(target_block=1, target_face=3, shape=5, face = 2, offset_x = -0.)
+    obs, r, done = env.step(action)
+
+    action = Action(target_block=0, target_face=0, shape=1, face = 0, offset_x = 1.5)
+    obs, r, done = env.step(action)
+
+    action = Action(target_block=3, target_face=3, shape=5, face = 1, offset_x = 0.)
+    obs, r, done = env.step(action)
+
+if 0:
+    action = Action(target_block=0, target_face=0, shape=1, face = 0, offset_x = -1.5)
+    obs, r, done = env.step(action)
+    print(r)
+    print(env.is_stable())
+    action = Action(target_block=1, target_face=3, shape=5, face = 2, offset_x = 0.)
+    obs, r, done = env.step(action)
+    print(r)
+    print(env.is_stable())
+    action = Action(target_block=0, target_face=0, shape=1, face = 0, offset_x = 1.5)
+    obs, r, done = env.step(action)
+    print(r)
+    print(env.is_stable())
+    action = Action(target_block=3, target_face=3, shape=5, face = 1, offset_x = 0.)
+    obs, r, done = env.step(action)
+    print(r)
+    print(env.is_stable())
+    action = Action(target_block=4, target_face=2, shape=5, face = 1, offset_x = 0.)
+    obs, r, done = env.step(action)
+    print(r)
+    print(env.is_stable())
+    print(done)
+
+
+plot_assembly_env(env, task=task)
+plt.axis('equal')
+plt.show()
